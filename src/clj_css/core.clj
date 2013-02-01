b9e30393cf307e961e1be2df0c875d118d4f19f3
Luke Crawford <luke0x@gmail.com>
2013-02-01 17:08:45 -0500

    Import of existing code

--- src/clj_css/core.clj
+++ src/clj_css/core.clj
@@ -1,6 +1,146 @@
-(ns clj-css.core)
+(ns clj-css.core
+  (:use [clojure.test]
+        [clojure.string :only [blank? trim]]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+; https://github.com/programble/csslj/blob/master/src/csslj/core.clj
+
+(defn- render-ele [ele]
+  (->> ele (map name) (interpose " ") (apply str)))
+
+(defn- render-attrs [attrs]
+  (apply str
+         (sort
+          (for [[attr value] attrs]
+            (str (name attr) ": " value "; ")))))
+
+(defn css
+  "Takes any amount of vectors of CSS elements and renders them as CSS"
+  ([vec]
+     (let [ele (render-ele (drop-last vec))
+           attrs (render-attrs (last vec))]
+       (str ele " { " attrs "}")))
+  ([vec & more]
+     (->> (map css (cons vec more)) (interpose " ") (apply str))))
+
+; [:page
+;   [:header-bar]
+;   [:project-title]
+;   [:project-toolbar]
+;   [:wiki-page
+;     [:code-example {:data-language "clojure"}
+;       code....
+;     ]
+;   ]
+; ]
+; (defcomponent header-bar [user]
+;   [
+;     [:home-link]
+;     [:notification-light]
+;     [:input { :name "search" }]
+;   ]
+; )
+; (defpallate-component header-bar
+; [][][][]][
+; )
+; define htmlrec protocol
+; define component protocol
+; covert html to htmlrec
+; convert htmlrec into html
+; convert component into htmlrec
+
+; (defn style-tree [style])
+; (defn merge-styles [styles])
+; (defn compact-styles [style])
+; (defn css-string [style])
+; 
+; (defprotocol Style
+;   (as-cljcss [this]))
+; 
+; (extend-protocol Style
+;   Object
+;   ([ :css ])
+; )
+; 
+; (defrecord CljStyle [value])
+; (extend-protocol Style
+;   (as-scss))
+; (defrecord SassStyle [a b])
+; 
+; (defrecord CssStyle [value])
+; (defrecord SassStyle [value])
+; (defrecord ScssStyle [value])
+; (defrecord GakaStyle [value])
+; 
+; ; css-clj
+; ; sass-clj
+; ; scss-clj
+; 
+; (def css-example
+; "
+;   #navbar {
+;     width: 80%;
+;     height: 23px;
+;   }
+;   #navbar ul {
+;     list-style-type: none;
+;   }
+;   #navbar li {
+;     float: left;
+;   }
+;   #navbar li a {
+;     font-weight: bold;
+;   }
+; ")
+; 
+; (def scss-example
+; "
+;   #navbar {
+;     width: 80%;
+;     height: 23px;
+; 
+;     ul {
+;       list-style-type: none;
+;     }
+;     li {
+;       float: left;
+;       a {
+;         font-weight: bold;
+;       }
+;     }
+;   }
+; ")
+; 
+; (def sass-example
+; "
+;   #navbar
+;     width: 80%;
+;     height: 23px;
+; 
+;     ul
+;       list-style-type: none;
+;     li
+;       float: left;
+;       a
+;         font-weight: bold;
+; ")
+
+; import all
+; convert foreign to native
+; convert native to foreign
+; export common
+
+(defn parse-css [css]
+  (if
+    (empty? css)
+    []
+    [:h1 {:color "#abcdef"}]))
+
+(defn emit-css [css-clj]
+  (if
+    (empty? css-clj)
+    ""
+    (trim (css css-clj))))
+
+(defn parse-scss [scss] [])
+(defn scss-to-css [scss] [])
+(defn css-to-scss [css] [])
--- test/clj_css/core_test.clj
+++ test/clj_css/core_test.clj
@@ -2,6 +2,29 @@
   (:use clojure.test
         clj-css.core))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest test-parse-css
+  (are [in out] (= (parse-css in) out)
+    "" []
+    "h1 { color: #abcdef; }" [:h1 {:color "#abcdef"}]
+    ; "h1 span { color: #abcdef; }" [:h1 :span {:color "#abcdef"}]
+    ))
+
+(deftest test-emit-css
+  (are [in out] (= (emit-css in) out)
+    [] ""
+    [:h1 {:color "#abcdef"}] "h1 { color: #abcdef; }"
+    [:h1 :span {:color "#abcdef"}] "h1 span { color: #abcdef; }"))
+
+(deftest test-parse-scss
+  (are [in out] (= (parse-scss in) out)
+    "" []))
+
+(deftest test-scss-to-css
+  (are [in out] (= (scss-to-css in) out)
+    [] []))
+
+(deftest test-css-to-scss
+  (are [in out] (= (css-to-scss in) out)
+    [] []))
+
+; (run-all-tests)
